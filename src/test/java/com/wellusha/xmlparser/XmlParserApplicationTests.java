31a57ccf78044c43ee01471ba5c762df4405635b
Vlad Shtender <Shtender.v.a@sberbank.ru>
2020-05-13 15:28:01 +0300

    added docker-compose.yml with spring datasource property

--- src/test/java/com/wellusha/xmlparser/XmlParserApplicationTests.java
+++ src/test/java/com/wellusha/xmlparser/XmlParserApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class XmlparserApplicationTests {
+class XmlParserApplicationTests {
 
     @Test
     void contextLoads() {
